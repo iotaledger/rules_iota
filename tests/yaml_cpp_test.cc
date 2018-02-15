fcc2bfad73299e13d8f7ff6c1224c30f654e1735
tsvi_sabo <tsvi.sabo@iota.org>
2018-02-15 18:47:10 +0200

    rules_iota: add yaml for cpp with a very little test

---
+++ tests/yaml_cpp_test.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+#include <yaml-cpp/yaml.h>
+
+int main(int argc, char** argv) {
+
+    auto conf = YAML::LoadFile("config_example.yaml");
+
+    std::cout<<"name: "<<conf["michael"]["name"]<<std::endl;
+    std::cout<<"job: "<<conf["michael"]["job"]<<std::endl;
+
+    std::cout<<"name: "<<conf["dwight"]["name"]<<std::endl;
+    std::cout<<"job: "<<conf["dwight"]["job"]<<std::endl;
+
+    return 0;
+}

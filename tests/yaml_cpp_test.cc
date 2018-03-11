631d7566c77dd0d9f3b6ab3ec7ad0eaef307ee0f
tsvi_sabo <tsvi.sabo@iota.org>
2018-03-11 15:59:18 +0200

    rules_iota/tests/yaml_cpp_test.cpp - clang

--- tests/yaml_cpp_test.cc
+++ tests/yaml_cpp_test.cc
@@ -1,16 +1,15 @@
 #include <iostream>
-
 #include <yaml-cpp/yaml.h>
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv) {
 
-    auto conf = YAML::LoadFile("config_example.yaml");
+  auto conf = YAML::LoadFile("config_example.yaml");
 
-    std::cout<<"name: "<<conf["michael"]["name"]<<std::endl;
-    std::cout<<"job: "<<conf["michael"]["job"]<<std::endl;
+  std::cout << "name: " << conf["michael"]["name"] << std::endl;
+  std::cout << "job: " << conf["michael"]["job"] << std::endl;
 
-    std::cout<<"name: "<<conf["dwight"]["name"]<<std::endl;
-    std::cout<<"job: "<<conf["dwight"]["job"]<<std::endl;
+  std::cout << "name: " << conf["dwight"]["name"] << std::endl;
+  std::cout << "job: " << conf["dwight"]["job"] << std::endl;
 
-    return 0;
+  return 0;
 }

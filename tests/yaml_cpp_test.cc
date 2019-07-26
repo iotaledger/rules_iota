896627184c7472b0065724a5807c12692e9d997c
Bing-Yang <51323441+bingyanglin@users.noreply.github.com>
2019-07-26 23:33:33 +0800

    Fix windows build prob: yaml, prometheus, mbedtls, libuv (#37)
    * add_test_cases
    
    * fix_return_values
    
    * fix windows build fails
    
    * modify windows cfg
    
    * remove -luv for libuv
    
    * fix yaml_cpp and yaml_test test fails for windows
    
    * fix libuv for linux

--- tests/yaml_cpp_test.cc
+++ tests/yaml_cpp_test.cc
@@ -1,15 +1,35 @@
 #include <iostream>
+#include <fstream>
 #include <yaml-cpp/yaml.h>
 
 int main(int argc, char **argv) {
+  YAML::Emitter emitter;
+  emitter << YAML::BeginMap;
+  emitter << YAML::Key << "michael";
+  emitter << YAML::Value << YAML::BeginMap;
+  emitter << YAML::Key << "name";
+  emitter << YAML::Value << "Michael Scott";
+  emitter << YAML::Key << "job";
+  emitter << YAML::Value << "Regional manager";
+  emitter << YAML::EndMap;
+  emitter << YAML::Key << "dwight";
+  emitter << YAML::Value << YAML::BeginMap;
+  emitter << YAML::Key << "name";
+  emitter << YAML::Value << "Dwight Schrute";
+  emitter << YAML::Key << "job";
+  emitter << YAML::Value << "Assitant to the regional manager";
+  emitter << YAML::EndMap;
+  emitter << YAML::EndMap;
 
-  auto conf = YAML::LoadFile("config_example.yaml");
+  std::ofstream fout("config_test_example.yaml");
+  fout << emitter.c_str();
+  fout.close();
+  auto conf = YAML::LoadFile("config_test_example.yaml");
 
   std::cout << "name: " << conf["michael"]["name"] << std::endl;
   std::cout << "job: " << conf["michael"]["job"] << std::endl;
 
   std::cout << "name: " << conf["dwight"]["name"] << std::endl;
   std::cout << "job: " << conf["dwight"]["job"] << std::endl;
-
   return 0;
 }

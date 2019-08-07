4cc46e0352b510aaa42671d0685cf65500b512ce
Tsvi Sabo <tsvi.sabo@iota.org>
2019-08-07 17:40:37 +0300

    boost/beast: add dummy test (#41)
    * boost/beast: add dummy test
    
    * boost/beast: test's dummy body

---
+++ tests/boost_beast_test.cc
@@ -0,0 +1,29 @@
+#include <boost/beast/core.hpp>
+#include <boost/beast/http.hpp>
+#include <boost/beast/ssl.hpp>
+#include <boost/beast/version.hpp>
+#include <boost/asio/connect.hpp>
+#include <boost/asio/ip/tcp.hpp>
+#include <boost/asio/ssl/error.hpp>
+#include <boost/asio/ssl/stream.hpp>
+#include <cstdlib>
+#include <iostream>
+#include <string>
+
+namespace beast = boost::beast; // from <boost/beast.hpp>
+namespace http = beast::http;   // from <boost/beast/http.hpp>
+namespace net = boost::asio;    // from <boost/asio.hpp>
+namespace ssl = net::ssl;       // from <boost/asio/ssl.hpp>
+using tcp = net::ip::tcp;       // from <boost/asio/ip/tcp.hpp>
+
+int main(int, char*[])
+{
+
+    // The io_context is required for all I/O
+    net::io_context ioc;
+
+    // The SSL context is required, and holds certificates
+    ssl::context ctx(ssl::context::tlsv12_client);
+
+    return 0;
+}
